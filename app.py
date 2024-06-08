3fc92191a72b6bc5f8277a90f68a008706cb5bae
Olivio Bafile <bafileliv@gmail.com>
2024-06-08 19:30:31 +0930

    Updated app.py - removed example.xls

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-# Simple python program 
+# This is a very simple program written in python
+# its purpose is just to mess around and learn
+
+# the print statement
+print(5*6)
 print('Hello World')
-my_list = ['switch', 'router', 'wireless access point']
+
+# Create a list
+my_list = [23,'Boliver', 'Road', 'Campbelltown', 'SA', '6054']
+
+# print my_list
 print(my_list)
-print(my_list[2])
-for x in my_list:
-    print(x)
 
