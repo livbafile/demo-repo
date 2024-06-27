0fd6ac44f70ad4e15994ed7f5bf47444876748c3
Olivio Bafile <bafileliv@gmail.com>
2024-06-27 23:33:14 +0930

    Added initial.py

---
+++ initial.py
@@ -0,0 +1,15 @@
+# Olivio Bafile
+# Play drums in several bands
+
+print(" OOO  BBBB")
+for x in range(0,2):
+  print("O   O B   B")
+print("O   O BBBB")
+for x in range(0,2):
+  print('O   O B   B')
+print(" OOO  BBBB")
+
+
+
+
+
